8f98898b7f4677805d8f5f69b91e5f94f2e759d6
Jon Shaw <jon.shaw@jassoft.co.uk>
2016-01-11 21:52:36 +0000

    added aws sdk and readme

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # aws-ecr-maven-plugin
+
+## Maven usage
+
+<plugin>
+  <groupId>com.jassoft.aws</groupId>
+  <artifactId>aws-ecr-maven-plugin</artifactId>
+  <version>1.0-SNAPSHOT</version>
+  <configuration>
+    <repositoryName>{Aws-Repo-URL}/ImageName</repositoryName>
+  </configuration>
+</plugin>
--- src/main/java/com/jassoft/aws/Deploy.java
+++ src/main/java/com/jassoft/aws/Deploy.java
@@ -1,8 +1,12 @@
 package com.jassoft.aws;
 
+import com.amazonaws.services.ecr.AmazonECR;
+import com.amazonaws.services.ecr.AmazonECRClient;
+import com.amazonaws.services.ecr.model.PutImageRequest;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
 
 /**
  * Created by jonshaw on 11/01/2016.
@@ -11,8 +15,16 @@ import org.apache.maven.plugins.annotations.Mojo;
 @Mojo( name = "deploy")
 public class Deploy extends AbstractMojo
 {
+    @Parameter
+    private String repositoryName;
+
     public void execute() throws MojoExecutionException
     {
-        getLog().info( "Hello, world." );
+        AmazonECR amazonECR = new AmazonECRClient();
+
+        PutImageRequest request = new PutImageRequest();
+        request.setRepositoryName(repositoryName);
+
+        amazonECR.putImage(request);
     }
 }
